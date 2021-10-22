87ab7ada1d974222e004582dffa1eefe843ba11b
DarkKronicle <darkkronicle@gmail.com>
2021-10-21 20:34:28 -0600

    Comments

--- example_filters/remove_black.js
+++ example_filters/remove_black.js
@@ -6,14 +6,16 @@ function setup(payload) {
 }
 
 
-// Remove's brackets from player names using *magic*
 function filter(payload) {
+    // Create new text to reconstruct from
     var newText = payload.getNewTextBuilder().build();
     var textSize = payload.getText().getRawTexts().size()
     for (var i = 0; i < textSize; i++) {
+        // Iterate through each
         var text = payload.getText().getRawTexts().get(i)
         var color = payload.getColor(text.getStyle())
         if (color != null && color.color() == 0) {
+            // If it's black change the color and append
             var textBuilder = payload.getNewTextBuilder(text.getString()).setStyle(text.getStyle())
             textBuilder.setColor(0x666666)
             newText.append(textBuilder.build())
@@ -21,5 +23,6 @@ function filter(payload) {
             newText.append(text)
         }
     }
+    // Build it and set it
     payload.setText(newText)
 }
